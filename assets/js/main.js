f6730f40051c530b8412535740c9987d30b93410
Arup-SMB <arupmoulick1@gmail.com>
2024-06-27 22:11:18 +0530

    Add Two New Pannel

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 7000)  
     
 });
+
+
+setInterval(showTime, 1000);
+
+
+// Defining showTime funcion
+function showTime() {
+	// Getting current time and date
+	let time = new Date();
+	let hour = time.getHours();
+	let min = time.getMinutes();
+	let sec = time.getSeconds();
+	
+
+	// Setting time for 12 Hrs format
+	if (hour >= 12) {
+		if (hour > 12) hour -= 12;
+		am_pm = "";
+	} else if (hour == 0) {
+		hr = 12;
+		am_pm = "";
+	}
+
+	hour =
+		hour < 10 ? "0" + hour : hour;
+	min = min < 10 ? "0" + min : min;
+	sec = sec < 10 ? "0" + sec : sec;
+
+	let currentTime =
+		hour +
+		":" +
+		min +
+		":" +
+		sec +
+		am_pm;
+
+	// Displaying the time
+	document.getElementById(
+		"clock"
+	).innerHTML = currentTime;
+}
+
+showTime();
